ea739e3acaa5551c442872c69509a92cfffa434b
Jeremy Hall <jeremyhall9804@gmail.com>
2020-12-23 21:09:17 -0500

    Enemies Added

--- src/Game.cpp
+++ src/Game.cpp
@@ -29,8 +29,8 @@ void main()
 	//GAME OBJECTS
 	const int N_players = 2;
 	Player* players[N_players];
-	Player player(600.0f, 300.0f, map, &SH, 1);
-	Player player2(500.0f, 200.0f, map, &SH, 2);
+	Player player(1300.0f, 300.0f, map, &SH, 1);
+	Player player2(400.0f, 200.0f, map, &SH, 2);
 	players[0] = &player;
 	players[1] = &player2;
 
