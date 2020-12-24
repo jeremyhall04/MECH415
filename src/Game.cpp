fc3d7e72f67611b9f5711d5b11eb4462e1c712d5
Nathan Egener <72566728+Noname101-pixel@users.noreply.github.com>
2020-12-23 20:34:54 -0500

    Added some comments

--- src/Game.cpp
+++ src/Game.cpp
@@ -39,9 +39,9 @@ void main()
 		player2.is_alive = false;
 	}
 
-	const int N_enemies = 6;
-	Enemy* p_enemies[N_enemies];
-	p_enemies[0] = new Smallboy(700.0f, 500.0f, map, &SH);
+	const int N_enemies = 6;//Declaring number of enemies
+	Enemy* p_enemies[N_enemies];//Declaring array of enemies
+	p_enemies[0] = new Smallboy(700.0f, 500.0f, map, &SH);//declaring enemy type and starting state
 	p_enemies[1] = new Turret(300.0f, 500.0f, map, &SH);
 	p_enemies[2] = new Turret(200.0f, 250.0f, map, &SH);
 	p_enemies[3] = new Largeboy(200.0f, 850.0f, map, &SH);
